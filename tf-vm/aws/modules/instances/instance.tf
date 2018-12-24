e47f57c51b1e3e267a92f16fe16d4a6128cb3806
Damien Laureaux <d.laureaux@timoa.com>
2018-12-24 07:43:06 +0100

    Add the public DNS output for the Jenkins master

--- tf-vm/aws/modules/instances/instance.tf
+++ tf-vm/aws/modules/instances/instance.tf
@@ -71,6 +71,11 @@ output "instance_public_ip" {
   value       = "${module.jenkins_master_ec2.public_ip[0]}"
 }
 
+output "instance_public_dns" {
+  description = "Public DNS assigned to the EC2 instance"
+  value       = "${module.jenkins_master_ec2.public_dns[0]}"
+}
+
 output "instance_az" {
   description = "Availability Zone where the EC2 instance is running"
   value       = "${module.jenkins_master_ec2.availability_zone}"
--- tf-vm/aws/outputs.tf
+++ tf-vm/aws/outputs.tf
@@ -2,3 +2,8 @@ output "jenkins_master_public_ip" {
   description = "Public IP assigned to the Jenkins master"
   value       = "${module.instances.instance_public_ip}"
 }
+
+output "jenkins_master_public_dns" {
+  description = "Public DNS assigned to the Jenkins master"
+  value       = "${module.instances.instance_public_dns}"
+}
