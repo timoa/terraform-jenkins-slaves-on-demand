3d90e8de9c5ccca7f6bd13c2641ee238017eda74
Damien Laureaux <d.laureaux@timoa.com>
2018-12-26 18:16:15 +0100

    Update AMI to Amazon Linux 2

--- tf-vm/aws/modules/instances/ami.tf
+++ tf-vm/aws/modules/instances/ami.tf
@@ -1,11 +1,33 @@
 data "aws_ami" "amazon_linux_2" {
+    description = "Latest Amazon Linux 2 AMI with SSD (GP2) uses for the Jenkins master"
     most_recent = true
 
     filter {
     name = "name"
 
     values = [
-      "amzn-ami-hvm-*-x86_64-gp2",
+      "amzn2-ami-hvm-*-x86_64-gp2",
+    ]
+  }
+
+  filter {
+    name = "owner-alias"
+
+    values = [
+      "amazon",
+    ]
+  }
+}
+
+data "aws_ami" "amazon_linux_2_minimal" {
+    description = "Latest Amazon Linux 2 AMI"
+    most_recent = true
+
+    filter {
+    name = "name"
+
+    values = [
+      "amzn2-ami-minimal-hvm-*-x86_64-ebs",
     ]
   }
 
