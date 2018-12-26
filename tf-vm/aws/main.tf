80924e74971e2bdb43cdc3df6cbe7a0ffa876273
Damien Laureaux <d.laureaux@timoa.com>
2018-12-26 17:04:36 +0100

    Improve documentation

--- tf-vm/aws/main.tf
+++ tf-vm/aws/main.tf
@@ -1,7 +1,11 @@
 module "network" {
   source = "./modules/network"
+
+  # Global
   env = "${var.env}"
   aws_region = "${var.aws_region}"
+
+  # VPC
   vpc_cidr = "${var.vpc_cidr}"
   public_subnet = "${var.public_subnet}"
   private_subnet = "${var.private_subnet}"
@@ -9,9 +13,15 @@ module "network" {
 
 module "instances" {
   source = "./modules/instances"
+
+  # Global
   env = "${var.env}"
+
+  # Network
   vpc_id = "${module.network.vpc_id}"
   public_subnets = "${module.network.public_subnets}"
+
+  # Instance
   security_group_id = "${module.network.jenkins_master_sg_id}"
   instance_type = "${var.instance_type}"
   ebs_optimized = "${var.ebs_optimized}"
